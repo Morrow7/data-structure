132eab03ca09850d897b67e16385ac39b0e00ca7
Morrow <susu997Y@gmail.com>
2026-01-23 20:47:11 +0800

    链表 哈希

---
+++ hash table/src/a2.java
@@ -0,0 +1,50 @@
+public class a2 {
+        class Node {
+            String key;
+            int value;
+            Node next;
+
+            Node(String key, int value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        Node[] table = new Node[10];
+
+        int hash(String key) {
+            return key.hashCode() % table.length;
+        }
+
+        void put(String key, int value) {
+            int index = hash(key);
+            Node cur = table[index];
+
+            if (cur == null) {
+                table[index] = new Node(key, value);
+                return;
+            }
+
+            while (true) {
+                if (cur.key.equals(key)) {
+                    cur.value = value;
+                    return;
+                }
+                if (cur.next == null) break;
+                cur = cur.next;
+            }
+            cur.next = new Node(key, value);
+        }
+
+        int get(String key) {
+            int index = hash(key);
+            Node cur = table[index];
+            while (cur != null) {
+                if (cur.key.equals(key)) return cur.value;
+                cur = cur.next;
+            }
+            return -1;
+        }
+    }
+
+
---
+++ linked list/src/a1.java
@@ -0,0 +1,28 @@
+public class a1 {
+   // ListNode cur=head;
+//    while(cur!=null){
+//        System.out.println(cur.val);
+//        cur=cur.next;
+//    }
+
+//    ListNode newNode=new ListNode(0);
+//    newNode.next=head;
+//    head=newNode;
+//    ListNode cur=head;
+//    while(cur.next!=null){
+//        if(cur.next.val==2){
+//            cur.next=cur.next.next;
+//            break;
+//        }
+//        cur=cur.next;
+//    }
+//    ListNode prev=null;
+//    ListNode cur=head;
+//    while(cur!=null){
+//            LsitNode next=vur.next;
+//            vur.next=prev;
+//            prec=cur;
+//            cur=next;
+//    }
+//    return prev;
+}
